c268695aa51e6fdaf64d7780ec59a61f458b34a1
gaborsornyei <info@sornyei.com>
2015-09-07 20:28:21 +0200

    Modal dialog added to Kipontozas, writing of documentation started

--- src/main/webapp/home/home.js
+++ src/main/webapp/home/home.js
@@ -7,17 +7,44 @@ angular.module('myApp.home', [])
             })
     })
 
-    .controller('KipontozCtrl', function ($scope, $http, $state, appConfig) {
-        $http.get(appConfig.baseUrl + 'common/kipontoz')
-            .success(function (data) {
-                toastr.success('A kipontozás befejeződött','', {
-                    timeOut:1000
-                });
-                $state.go('home')
-            })
+    .controller('KipontozCtrl', function ($scope, $http, $state, $modal, appConfig) {
+
+        var init = function () {
+            var modalInstance = $modal.open({
+                animation: false,
+                templateUrl: 'home/kipontoz.modal.html',
+                controller: 'KipontozMdlCtrl'
+            });
+
+            modalInstance.result
+                .then(function () {
+                    $http.get(appConfig.baseUrl + 'common/kipontoz')
+                        .success(function (data) {
+                            toastr.success('A kipontozás befejeződött', '', {
+                                timeOut: 1000
+                            });
+                            $state.go('home')
+                        })
+                })
+        }
+        init();
     })
 
     .controller('HomeCtrl', function ($scope) {
 
+    })
+
+    .controller('KipontozMdlCtrl', function ($scope, $state, $modalInstance) {
+        $scope.ok = function () {
+            $modalInstance.close();
+        }
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss();
+            toastr.info('Kipontozás megszakítva', '', {
+                timeOut: 1000
+            })
+            $state.go('home');
+        }
     })
 
